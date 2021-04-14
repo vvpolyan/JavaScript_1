fb56e6bff30a2c7da45e3b55669ed36fc9ddce65
vvpolyan <vireshnik@mail.ru>
2021-04-14 11:15:32 +0300

    Домашнее задание 1

---
+++ Lesson_1_Home_Work/home_work_1.js
@@ -0,0 +1,12 @@
+//"use script";
+
+var tc = 28;
+var tf = (9 / 5) * tc + 32;
+alert("Задание_1: Температура по Форингейту " + tf);
+
+var admin = "Василий";
+var name = admin;
+alert("Задание_2: name = " + name);
+
+var num = 1000 + "108";
+alert("Задание_3: num = " + num);
